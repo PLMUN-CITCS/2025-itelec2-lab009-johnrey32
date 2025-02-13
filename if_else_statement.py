e815d0482310a727a038ff806be5ccd6e3eb623c
johnrey32 <bulfajohnrey_bsit@plmun.edu.ph>
2025-02-13 22:02:06 +0800

    Submitting Python Week 04 - Laboratory # 09

---
+++ if_else_statement.py
@@ -0,0 +1,16 @@
+# John Rey Bulfa
+# ITELEC2
+# Laboratory #09 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+    user_input = input("Enter a number: ")
+    number = int(user_input)
+    if number % 2 == 0:
+        print("The number", number, "is Even.")
+    else:
+        print("The number", number, "is Odd.")
+        print("Invalid input. Please enter an integer.")
+
+if __name__ == "__main__":
+    main()
